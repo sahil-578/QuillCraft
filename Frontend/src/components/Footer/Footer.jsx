30f7cea94a80047dfe88d8cd59510a171c8d9970
Sahil Sharma <32904404+sahil-578@users.noreply.github.com>
2024-12-20 00:53:54 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -12,12 +12,12 @@ function Footer() {
       <div className='h-fit w-full p-4 flex'>
         <div className='h-fit w-[60%] flex ml-[5rem]'>
           <div className='h-fit flex items-center w-full'>
-            <Link to={'https://github.com/el3c-tron'} target='_blank'>
+            <Link to={'https://github.com/sahil-578'} target='_blank'>
               <div className='fill-none stroke-white stroke-[8] p-2 ml-10 opacity-40 hover:opacity-80 hover:stroke-white hover:stroke-[10] transition-all ease-in-out duration-200 cursor-pointer'>
                 <Github />
               </div>
             </Link>
-            <Link to={'https://www.linkedin.com/in/aniket-kumar-421891247/'} target='_blank'>
+            <Link to={'https://www.linkedin.com/in/sahil-sharma-3b691b230/'} target='_blank'>
               <div className='fill-none stroke-white opacity-40 stroke-[10] p-2 ml-10 hover:opacity-80 hover:stroke-white hover:stroke-[12] transition-all ease-in-out duration-200 cursor-pointer'>
                 <LinkedIn />
               </div>
@@ -35,7 +35,7 @@ function Footer() {
               </div>
           </Link>
           <span className='flex text-xs tracking-widest text-[#b3b3b3] opacity-30'>
-            All rights reserved by Aniket Kumar
+            All rights reserved by Sahil Sharma
           </span>
         </div>
       </div>
